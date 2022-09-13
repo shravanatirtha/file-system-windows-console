71b7186ae0f55d407edb78bfcad49d008ba73c20
shravanatirtha <shravz.ceo@gmail.com>
2022-09-13 13:09:29 +0530

    succesffully built stable version of filesystem utility

--- FileSystem.c
+++ FileSystem.c
@@ -30,7 +30,7 @@
 #include "FileSystem.h"
 #include <windows.h>
 #include <dirent.h>
-JNIEXPORT void JNICALL Java_FileSystem_fileSystem(JNIEnv *env, jobject clsobj, jstring path)
+JNIEXPORT jobject JNICALL Java_FileSystem_fileSystem(JNIEnv *env, jobject clsobj, jstring path)
 {
     int directoryCount = 0;
     int fileCount = 0;
@@ -43,14 +43,14 @@ JNIEXPORT void JNICALL Java_FileSystem_fileSystem(JNIEnv *env, jobject clsobj, j
     if ((path_handle = FindFirstFile(file_path, &data_file)) == INVALID_HANDLE_VALUE)
     {
         printf("Cannot be accessed: [%s]\n", pathName);
-        return;
+        return NULL;
     }
 
     while (FindNextFile(path_handle, &data_file))
     {
         if (strcmp(data_file.cFileName, ".") != 0 && strcmp(data_file.cFileName, "..") != 0 && (strcmp(data_file.cFileName, ".git") != 0 && strcmp(data_file.cFileName, ".vscode") != 0) && (strcmp(data_file.cFileName, ".config") != 0 && strcmp(data_file.cFileName, "System Volume Information") != 0))
         {
-            sprintf(file_path, "%s\\%s", path, data_file.cFileName);
+            sprintf(file_path, "%s\\%s", pathName, data_file.cFileName);
             if (data_file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
             {
                 directoryCount++;
@@ -62,13 +62,19 @@ JNIEXPORT void JNICALL Java_FileSystem_fileSystem(JNIEnv *env, jobject clsobj, j
             }
         }
     }
-    printf("path: %s\n", pathName);
-    printf("directoryCount: %d\n", directoryCount);
-    printf("fileCount: %d\n", fileCount);
-    printf("totalSize: %d\n", totalSize);
     FindClose(path_handle);
-    (*env)->ReleaseStringUTFChars(env, path, pathName);
+    jclass cls = (*env)->FindClass(env, "FileInfo");
+    jmethodID mid = (*env)->GetMethodID(env, cls, "<init>", "(Ljava/lang/String;III)V");
+    jstring jstr = (*env)->NewStringUTF(env, pathName);
+    jobject obj = (*env)->NewObject(env, cls, mid, jstr, directoryCount, fileCount, totalSize);
+    return obj;
 }
+// printf("path: %s\n", pathName);
+// printf("directoryCount: %d\n", directoryCount);
+// printf("fileCount: %d\n", fileCount);
+// printf("totalSize: %d\n", totalSize);
+// FindClose(path_handle);
+// (*env)->ReleaseStringUTFChars(env, path, pathName);
 // WIN32_FIND_DATA wfd;
 // HANDLE hFind = INVALID_HANDLE_VALUE;
 // DWORD dwError = 0;
