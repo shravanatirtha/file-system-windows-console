10605ae3b12a91742d99d07b9cd5b19c846bdc4f
shravanatirtha <shravz.ceo@gmail.com>
2022-09-19 19:40:35 +0530

    added update feature

--- FileSystem.java
+++ FileSystem.java
@@ -1,7 +1,7 @@
 
 // Steps to execute
 // javac -h . FileSystem.java
-// gcc -I"C:\Program Files\Java\jdk1.8.0_202\include" -I"C:\Program Files\Java\jdk1.8.0_202\include\win32" -shared -o FileSys.dll FileSystem.c
+// gcc -I"C:\Program Files\Java\jdk1.8.0_333\include" -I"C:\Program Files\Java\jdk1.8.0_333\include\win32" -shared -o FileSys.dll FileSystem.c
 // java FileSystem
 
 /*
@@ -25,46 +25,189 @@ If you are going to pass a lot of such primitive objects to and from java, then 
  * 
  * 
  */
-
-import java.util.*;
-// import java.sql.Connection;
-// import java.sql.DriverManager;
-// import java.sql.ResultSet;
-// import java.sql.SQLException;
-// import java.sql.Statement;
+// file utility class
+// jdbc class
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class FileSystem {
-
+    // load library path of dll
     static {
         System.loadLibrary("FileSys");
     }
 
+    // native method
     private native FileInfo fileSystem(String path);
 
+    // jdbc connectivity variables
+    private static String jdbc_class = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/file_info";
+    private static String user = "root";
+    private static String password = "";
+
     public static void main(String[] args) {
-        FileSystem fs = new FileSystem();
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the path");
-        String path = sc.nextLine();
-        FileInfo fi = fs.fileSystem(path);
-        System.out.println("File Name: " + fi.getcFileName());
-        System.out.println("Directory Count: " + fi.getDirectory_count());
-        System.out.println("File Count: " + fi.getFile_count());
-        System.out.println("File Size: " + fi.getnFileSizeLow());
-        // fs.fileSystem(path);
-        // FileInfo fi = fs.fileSystem(path);
-        // System.out.println("File Name: " + fi.getcFileName());
-        // System.out.println("Directory Count: " + fi.getDirectory_count());
-        // System.out.println("File Count: " + fi.getFile_count());
-        // System.out.println("File Size: " + fi.getnFileSizeLow());
-        sc.close();
+
+        try {
+
+            FileSystem fs = new FileSystem();
+            System.out.println("Enter the path");
+            String path = sc.nextLine();
+            path=path.toLowerCase();
+            FileInfo fi = fs.fileSystem(path);
+            System.out.println("File Name: " + fi.getcFileName());
+            System.out.println("Directory Count: " + fi.getDirectory_count());
+            System.out.println("File Count: " + fi.getFile_count());
+            String size = "";
+            Long sizeInBytes = new Long(fi.getnFileSizeLow());
+            if (sizeInBytes < 1024) {
+                size = Long.toString(sizeInBytes) + " Bytes";
+            } else if (sizeInBytes >= 1024 && sizeInBytes < 1048576) {
+                size = Long.toString(sizeInBytes / 1024) + " KB";
+            } else if (sizeInBytes >= 1048576 && sizeInBytes < 1073741824) {
+                size = Long.toString(sizeInBytes / 1048576) + " MB";
+            } else if (sizeInBytes >= 1073741824) {
+                size = Long.toString(sizeInBytes / 1073741824) + " GB";
+            }
+            System.out.println("File Size: " + size);
+            Class.forName(jdbc_class);
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = con.prepareStatement("select * from directory WHERE path=?");
+            stmt.setString(1, path);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                update(fi, size);
+            } else {
+
+                insert(fi, size);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            sc.close();
+        }
     }
 
+    static void insert(FileInfo fi, String size) {
+        try {
+            Class.forName(jdbc_class);
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement
+
+            stmt = con.prepareStatement("insert into directory values(?,?,?,?)");
+            stmt.setString(1, fi.getcFileName());
+            stmt.setInt(2, fi.getDirectory_count());
+            stmt.setInt(3, fi.getFile_count());
+            stmt.setString(4, size);
+            stmt.executeUpdate();
+
+            System.out.println("Directory inserted successfully");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static void update(FileInfo fi, String size) {
+        try {
+            Class.forName(jdbc_class);
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = con
+                    .prepareStatement("update directory set subdirectories=?,files=?,size=? where path=?;");
+            stmt.setString(1, fi.getcFileName());
+            stmt.setInt(2, fi.getDirectory_count());
+            stmt.setInt(3, fi.getFile_count());
+            stmt.setString(4, size);
+            stmt.executeUpdate();
+            System.out.println("Data updated successfully");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
-// private static String jdbc_class = "com.mysql.cj.jdbc.Driver";
-// private static String url = "jdbc:mysql://localhost:3306/file_info";
-// private static String user = "root";
-// private static String password = "";
+
+// stmt.setString(1, path);
+// ResultSet result = stmt.executeQuery();
+
+// if (result.next()) {
+// stmt = con.prepareStatement("insert into directory values(?,?,?,?);");
+// stmt.setString(1, fi.getcFileName());
+// stmt.setInt(2, fi.getDirectory_count());
+// stmt.setInt(3, fi.getFile_count());
+// stmt.setString(4, size);
+// stmt.executeUpdate();
+// System.out.println("Data inserted successfully");
+// stmt.close();
+// } else {
+// stmt = con.prepareStatement("update directory set
+// subdirectories=?,files=?,size=? where path=?;");
+// stmt.setString(1, path);
+// stmt.setInt(2, fi.getDirectory_count());
+// stmt.setInt(3, fi.getFile_count());
+// stmt.setString(4, size);
+// stmt.executeUpdate();
+// System.out.println("Data updated successfully");
+// stmt.close();
+// }
+
+// con.close();
+// } catch (ClassNotFoundException | SQLException e) {
+// e.printStackTrace();
+// } finally {
+// sc.close();
+// }
+// }
+// }
+// stmt.setString(1, path);
+// result = stmt.executeUpdate();
+// if (result == 0) {
+// stmt = con.prepareStatement("insert into file_info.directory
+// values(?,?,?,?)");
+// stmt.setString(1, fi.getcFileName());
+// stmt.setInt(2, fi.getDirectory_count());
+// stmt.setInt(3, fi.getFile_count());
+// stmt.setString(4, size);
+// stmt.executeUpdate();
+// if (result == 1) {
+// System.out.println("Data inserted successfully");
+// } else {
+// System.out.println("Data not inserted");
+// }
+// } else {
+// stmt = con.prepareStatement(
+// "update directory set path=?, directory_count=?, file_count=?, size=? WHERE
+// path=?");
+// stmt.setString(1, fi.getcFileName());
+// stmt.setInt(2, fi.getDirectory_count());
+// stmt.setInt(3, fi.getFile_count());
+// stmt.setString(4, size);
+// stmt.setString(5, path);
+// result = stmt.executeUpdate();
+// if (result == 1) {
+// System.out.println("Data updated successfully");
+// } else {
+// System.out.println("Data not updated");
+// }
+// }
+
+// con.close();
+// } catch (ClassNotFoundException | SQLException e) {
+// e.printStackTrace();
+// } finally {
+// sc.close();
+// }
 
 // private native FileInfo fileSystemUtility(String path);
 
@@ -96,20 +239,7 @@ public class FileSystem {
 // System.out.println("Files: " + fi.getFiles());
 // System.out.println("Size: " + fi.getSize());
 // fs.callback();
-// try {
-// Class.forName(jdbc_class);
-// Connection con = DriverManager.getConnection(url, user, password);
-// Statement stmt = con.createStatement();
-// String sql = "INSERT INTO file_info (path, subdirectories, files, size)
-// VALUES ('" + fi.getPath() + "', "
-// + fi.getSubdirectories() + ", " + fi.getFiles() + ", " + fi.getSize() + ")";
-// stmt.executeUpdate(sql);
-// con.close();
-// } catch (ClassNotFoundException | SQLException e) {
-// e.printStackTrace();
-// }
-// }
-// }
+//
 
 // FileInfo fileInfo = new FileSystem().fileSystemUtility(path);
 // fileInfo.setPath(path);
