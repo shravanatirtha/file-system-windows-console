6e220cb74846972bde8492e4b35b7bae102f5cb2
shravanatirtha <shravz.ceo@gmail.com>
2022-09-21 12:59:03 +0530

    fixed version 7

--- file-system-versions/v7.c
+++ file-system-versions/v7.c
@@ -1,16 +1,13 @@
 /*This program prints the files names of the directory given by user recursively*/
 #include <stdio.h>
 #include <windows.h>
-// void display(char *files);
 int main()
 {
     printf("\n--------------------------------------------------------------------\n");
     printf("\nFile system information\n");
     printf("\n--------------------------------------------------------------------\n");
-    // printf("\nDirectory: \n");
-    // printf("\n--------------------------------------------------------------------\n");
     int size = 0;
-    size = display("c:\\users\\", 0, 0, 0);
+    size = path_size("E:\\file-system-using-c", 0);
     if (size < 1024)
     {
         printf("\tSize: %d bytes\n", size);
@@ -27,10 +24,16 @@ int main()
     {
         printf("\tSize: %d GB\n", size / (1024 * 1024 * 1024));
     }
-    printf("%d", size);
+    int file_count = 0;
+    file_count = files("E:\\file-system-using-c", 0);
+    printf("\tNumber of files: %d\n", file_count);
+    int dir_count = 0;
+    dir_count = directories("E:\\file-system-using-c", 0);
+    printf("\tNumber of directories: %d\n", dir_count);
     return 0;
 }
-int display(char *path, int directory_count, int file_count, int size)
+
+int path_size(char *path, int size)
 {
     char file_path[1000];
     sprintf(file_path, "%s\\*.*", path);
@@ -38,7 +41,7 @@ int display(char *path, int directory_count, int file_count, int size)
     WIN32_FIND_DATA data_file;
     if ((path_handle = FindFirstFile(file_path, &data_file)) == INVALID_HANDLE_VALUE)
     {
-        //  printf("Cannot be accessed: [%s]\n", path);
+        // printf("Cannot be accessed: [%s]\n", path);
     }
 
     while (FindNextFile(path_handle, &data_file))
@@ -48,21 +51,81 @@ int display(char *path, int directory_count, int file_count, int size)
             sprintf(file_path, "%s\\%s", path, data_file.cFileName);
             if (data_file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
             {
-                // printf("\nDirectory Path: %d - %s\n", directory_count, file_path);
-                directory_count++;
-                size=size+display(file_path, directory_count, file_count, size);
+
+                size = size + path_size(file_path, size);
             }
             else
             {
-                // printf("File:  %d - %s\n", file_count, file_path);
-                file_count++;
+
                 size += data_file.nFileSizeLow;
-                // display(file_path, directory_count, file_count, size);
             }
         }
     }
-    
 
     FindClose(path_handle);
+    if (size < 0)
+        size = abs(size);
     return size;
+}
+
+int files(char *path, int file_count)
+{
+    char file_path[1000];
+    sprintf(file_path, "%s\\*.*", path);
+    HANDLE path_handle = NULL;
+    WIN32_FIND_DATA data_file;
+    if ((path_handle = FindFirstFile(file_path, &data_file)) == INVALID_HANDLE_VALUE)
+    {
+        // printf("Cannot be accessed: [%s]\n", path);
+    }
+
+    while (FindNextFile(path_handle, &data_file))
+    {
+        if (strcmp(data_file.cFileName, ".") != 0 && strcmp(data_file.cFileName, "..") != 0 && (strcmp(data_file.cFileName, ".git") != 0 && strcmp(data_file.cFileName, ".vscode") != 0) && (strcmp(data_file.cFileName, ".config") != 0 && strcmp(data_file.cFileName, "System Volume Information") != 0))
+        {
+            sprintf(file_path, "%s\\%s", path, data_file.cFileName);
+            if (data_file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                file_count = files(file_path, file_count);
+            }
+            else
+            {
+                file_count++;
+            }
+        }
+    }
+
+    FindClose(path_handle);
+    return file_count;
+}
+
+int directories(char *path, int directory_count)
+{
+    char file_path[1000];
+    sprintf(file_path, "%s\\*.*", path);
+    HANDLE path_handle = NULL;
+    WIN32_FIND_DATA data_file;
+    if ((path_handle = FindFirstFile(file_path, &data_file)) == INVALID_HANDLE_VALUE)
+    {
+        // printf("Cannot be accessed: [%s]\n", path);
+    }
+
+    while (FindNextFile(path_handle, &data_file))
+    {
+        if (strcmp(data_file.cFileName, ".") != 0 && strcmp(data_file.cFileName, "..") != 0 && (strcmp(data_file.cFileName, ".git") != 0 && strcmp(data_file.cFileName, ".vscode") != 0) && (strcmp(data_file.cFileName, ".config") != 0 && strcmp(data_file.cFileName, "System Volume Information") != 0))
+        {
+            sprintf(file_path, "%s\\%s", path, data_file.cFileName);
+            if (data_file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+
+                directory_count = directories(file_path, directory_count);
+                directory_count++;
+            }
+            else
+            {
+            }
+        }
+    }
+    FindClose(path_handle);
+    return directory_count;
 }
