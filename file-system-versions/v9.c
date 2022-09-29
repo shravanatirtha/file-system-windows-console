e430fb1b6bba626cf3861e7e42f8269cab2d5cec
shravanatirtha <shravz.ceo@gmail.com>
2022-09-29 16:10:24 +0530

    fixed version bugs

--- file-system-versions/v9.c
+++ file-system-versions/v9.c
@@ -1,28 +1,22 @@
-/*This program prints the files names of the directory given by user recursively with the path and number of files and directories in it*/
+/*This program prints the files names of the directory given by user path*/
 #include <stdio.h>
 #include <dirent.h>
 #include <windows.h>
-void display(char *path, int directory_count, int file_count);
+void display(char *path);
 char **ref;
 int j = 0;
 int main()
 {
-    // char path[1000];
-    // scanf("%s", path);
-    // int directory_count = 1;
-    // int file_count = 1;
-    // display("E:\\", directory_count, file_count);
-    printf("\n--------------------------------------------------------------------\n");
+    printf("\n-----------------------------------------\n");
     printf("\nFile system information\n");
-    printf("\n--------------------------------------------------------------------\n");
+    printf("\n-----------------------------------------\n");
     ref = (char **)malloc(sizeof(char *) * 29);
-    display("E:\\", 0, 0);
-    printf("\n--------------------------------------------------------------------\n");
+    display("E:\\");
     for (int i = 0; i < 29; i++)
-        printf("%d\t %s\n",i, ref[i]);
+        printf("%d\t %s\n", i, ref[i]);
     return 0;
 }
-void display(char *path, int directory_count, int file_count)
+void display(char *path)
 {
     char file_path[1000];
     sprintf(file_path, "%s\\*.*", path);
@@ -30,10 +24,8 @@ void display(char *path, int directory_count, int file_count)
     WIN32_FIND_DATA data_file;
     if ((path_handle = FindFirstFile(file_path, &data_file)) == INVALID_HANDLE_VALUE)
     {
-        printf("Path not found: [%s]\n", path);
+        // printf("Path not found: [%s]\n", path);
     }
-    // char **ref = (char **)malloc(sizeof(char *) * 29);
-    // int i = 0;
     while (FindNextFile(path_handle, &data_file))
     {
         if (strcmp(data_file.cFileName, ".") != 0 && strcmp(data_file.cFileName, "..") != 0 && strcmp(data_file.cFileName, ".metadata") != 0 && strcmp(data_file.cFileName, ".settings") != 0 && (strcmp(data_file.cFileName, ".git") != 0 && strcmp(data_file.cFileName, ".vscode") != 0) && (strcmp(data_file.cFileName, ".config") != 0 && strcmp(data_file.cFileName, "System Volume Information") != 0))
@@ -41,22 +33,12 @@ void display(char *path, int directory_count, int file_count)
             sprintf(file_path, "%s\\%s", path, data_file.cFileName);
             if (data_file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
             {
-                // printf("%s\n", file_path);
                 ref[j] = (char *)malloc(1000 * sizeof(char));
                 strcpy(ref[j], file_path);
-                printf("%s\n", ref[j]);
                 j++;
-                directory_count++;
-
-                display(file_path, directory_count, file_count);
-            }
-            else
-            {
-                // printf("File:  %d - %s\n", file_count, file_path);
-                file_count++;
+                display(file_path);
             }
         }
     }
     FindClose(path_handle);
-    // return ref;
 }
