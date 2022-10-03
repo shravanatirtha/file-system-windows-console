6acedd6a563fe551a248434af8d34755b8f44204
shravanatirtha <shravz.ceo@gmail.com>
2022-10-03 14:31:00 +0530

    db structure modified

--- data.sql
+++ data.sql
@@ -1,11 +1,12 @@
 create database if not exists `file_info`;
 use file_info;
 create table directory (
-  path varchar(255) not null,
+  parent_path varchar(255) not null,
+  directory_name varchar(255) not null,
   subdirectories int(100) not null,
   files int(100) not null,
   size_bytes varchar(100) not null,
   size int(100) not null,
   sync_time timestamp not null default current_timestamp,
-  version int(100) not null,
+  version int(100) not null
 );
